ea4033be4004e074b19875e54a137985594ce07b
Sean Murphy <sean@iamseanmurphy.com>
2024-10-01 01:11:22 +0000

    2.0.0

---
+++ mailster-woocommerce/build/trigger/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => 'e6de0efb9c2019cbf7c4');
